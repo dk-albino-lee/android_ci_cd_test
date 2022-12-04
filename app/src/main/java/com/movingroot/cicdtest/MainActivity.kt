1b2386a49f7625a780613f43e79e3d0e514d4780
dk-albino-lee <dk4620@naver.com>
2022-12-04 22:44:59 +0900

    try modify activity

--- app/src/main/java/com/movingroot/cicdtest/MainActivity.kt
+++ app/src/main/java/com/movingroot/cicdtest/MainActivity.kt
@@ -23,7 +23,7 @@ class MainActivity : ComponentActivity() {
                     color = MaterialTheme.colors.background
                 ) {
                     Greeting("World")
-                    TestDate(date = "20221202 night")
+                    TestDate(date = "20221204 night")
                 }
             }
         }
