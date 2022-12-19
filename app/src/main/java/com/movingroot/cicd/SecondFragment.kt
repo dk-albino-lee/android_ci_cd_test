1a2ee0bfcdf0b65f721898e3736af98c71f1812f
dk-albino-lee <dk4620@naver.com>
2022-12-19 16:11:19 +0900

    build succeeded in android studio terminal. why?

--- app/src/main/java/com/movingroot/cicd/SecondFragment.kt
+++ app/src/main/java/com/movingroot/cicd/SecondFragment.kt
@@ -24,7 +24,7 @@ class SecondFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        Log.d("Hello", "World")
+        Log.d("Hello", "World 2")
         _binding = FragmentSecondBinding.inflate(inflater, container, false)
         return binding.root
 
