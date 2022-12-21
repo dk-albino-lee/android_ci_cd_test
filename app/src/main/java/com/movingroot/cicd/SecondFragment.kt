47d2e2791e78ca80307e04ced29020edb2ae5a3a
dk-albino-lee <dk4620@naver.com>
2022-12-21 23:23:59 +0900

    change app id

--- app/src/main/java/com/movingroot/cicd/SecondFragment.kt
+++ app/src/main/java/com/movingroot/cicd/SecondFragment.kt
@@ -24,7 +24,7 @@ class SecondFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        Log.d("Hello", "World 5")
+        Log.d("Hello", "World 6")
         _binding = FragmentSecondBinding.inflate(inflater, container, false)
         return binding.root
 
